ad1922717fb89d5976c5cfbde4ff2beaab5abf5a
Jonathan Gaul <jon@gaul.tech>
2025-06-09 07:06:14 +0000

    Initial commit

---
+++ Function.cs
@@ -0,0 +1,48 @@
+
+using System.Text.Json.Serialization;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using Amazon.Lambda.SQSEvents;
+using WordList.Common.Messaging;
+using WordList.Data.Sql;
+using WordList.Data.Sql.Models;
+
+public class Function
+{
+    private readonly static WordDb s_wordDb = new();
+
+    public static async Task<string> FunctionHandler(SQSEvent input, ILambdaContext context)
+    {
+        var log = new LambdaContextLogger(context);
+
+        var messages = MessageQueues.UpdateWords.Receive(input, log);
+
+        var words = messages.Select(message => new Word
+        {
+            Text = message.Word,
+            Offensiveness = message.Offensiveness,
+            Commonness = message.Commonness,
+            Sentiment = message.Sentiment,
+            WordTypes = message.WordTypes
+        });
+
+        await s_wordDb.UpsertWordsAsync([.. words]).ConfigureAwait(false);
+
+        return "ok";
+    }
+
+    public static async Task Main()
+    {
+        Func<SQSEvent, ILambdaContext, Task<string>> handler = FunctionHandler;
+        await LambdaBootstrapBuilder.Create(handler, new SourceGeneratorLambdaJsonSerializer<LambdaFunctionJsonSerializerContext>())
+            .Build()
+            .RunAsync();
+    }
+}
+
+[JsonSerializable(typeof(string))]
+[JsonSerializable(typeof(SQSEvent))]
+public partial class LambdaFunctionJsonSerializerContext : JsonSerializerContext
+{
+}
